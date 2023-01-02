d3428198d71bd924efe0c8857c1b306a86d027aa
Reih02 <reillyhaskins@gmail.com>
2023-01-02 18:42:21 +1300

    linked css and started on HomeContainer class

---
+++ src/components/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class HomeContainer extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>What's next?</h1>
+            </div>
+        )
+    }
+}
+
+export default HomeContainer
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+// component files
+import HomeContainer from "./components/HomeContainer"
+
+//stylesheet
+import "./App.css"
+
 ReactDOM.render(
+    <HomeContainer />,
     document.getElementById("root")
 )
