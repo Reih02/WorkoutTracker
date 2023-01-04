cdcb6c35ffb5cc9ba20568b313796084c348ca44
Reilly Haskins <reillyhaskins@gmail.com>
2023-01-05 12:58:32 +1300

    implemented 404 error page to handle non-existent urls

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 // component files
 import HomeContainer from "./pages/home/components/HomeContainer"
 import WorkoutContainer from "./pages/workouts/WorkoutContainer"
+import NoMatch from "./pages/nomatch/NoMatch"
 import Navbar from "./pages/home/components/Navbar"
 
 //stylesheet
@@ -15,6 +16,7 @@ ReactDOM.render(
         <Routes>
             <Route path="/" element={<HomeContainer />} />
             <Route path="/workouts" element={<WorkoutContainer />} />
+            <Route path="*" element={<NoMatch />} />
         </Routes>
     </BrowserRouter>,
     document.getElementById("root")
